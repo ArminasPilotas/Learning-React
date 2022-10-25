cfd89acfc4a5f9d4b7e7a451be1863be390e49e7
Arminas Pilotas <apilotas@reiz.tech>
2022-10-25 18:35:40 +0300

    Split AddToCardButton to separate components to isolate state

--- eshop/src/product/components/AddToCardButton.jsx
+++ eshop/src/product/components/AddToCardButton.jsx
@@ -1,11 +1,30 @@
 import Button, { buttonTypes } from "../../common/components/Button";
+import useLoadingDots from "../../common/hooks/useLoadingDots";
 
-function AddToCardButton({ isLoading, onClick }) {
+function Btn({ onClick }) {
   return (
-    <Button type={buttonTypes.Primary} disabled={isLoading} onClick={onclick}>
-      {isLoading ? "ADDING TO CARD" : "ADD TO CARD"}
+    <Button type={buttonTypes.PRIMARY} onClick={onclick}>
+      Add To Card
     </Button>
   );
+}
+
+function LoadingBtn() {
+  const loadingDots = useLoadingDots();
+
+  return (
+    <Button type={buttonTypes.PRIMARY} disabled className="flex">
+      <span>Adding To Card</span>
+      <span className="w-3 text-left">{loadingDots}</span>
+    </Button>
+  );
+}
+
+function AddToCardButton({ isLoading, onClick }) {
+  if (isLoading) {
+    return <LoadingBtn />;
+  }
+  return <Btn onClick={onClick} />;
 }
 
 export default AddToCardButton;
