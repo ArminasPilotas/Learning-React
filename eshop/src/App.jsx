24d5e79b7ab166e6c56e143e9a8212c06e7a0ca4
Arminas Pilotas <apilotas@reiz.tech>
2022-10-25 19:11:17 +0300

    Use cart context in cart nav item

--- eshop/src/App.jsx
+++ eshop/src/App.jsx
@@ -5,22 +5,25 @@ import Product from "./product/Product";
 import Vision from "./vision/Vision";
 import ContactUs from "./contact-us/ContactUs";
 import Cart from "./cart/Cart";
+import CartProvider from "./common/providers/CartProvider";
 
 function App() {
   return (
     <Router>
-      <header>
-        <NavBar />
-      </header>
-      <main className="p-2" style={{ height: "calc(100vh - 48px" }}>
-        <Switch>
-          <Route exact path={["/", "/products"]} component={Products} />
-          <Route exact path="/product/:id" component={Product} />
-          <Route exact path="/vision" component={Vision} />
-          <Route exact path="/contact" component={ContactUs} />
-          <Route exact path="/cart" component={Cart} />
-        </Switch>
-      </main>
+      <CartProvider>
+        <header>
+          <NavBar />
+        </header>
+        <main className="p-2" style={{ height: "calc(100vh - 48px" }}>
+          <Switch>
+            <Route exact path={["/", "/products"]} component={Products} />
+            <Route exact path="/product/:id" component={Product} />
+            <Route exact path="/vision" component={Vision} />
+            <Route exact path="/contact" component={ContactUs} />
+            <Route exact path="/cart" component={Cart} />
+          </Switch>
+        </main>
+      </CartProvider>
     </Router>
   );
 }
--- eshop/src/nav-bar/components/CartNavItem.jsx
+++ eshop/src/nav-bar/components/CartNavItem.jsx
@@ -1,12 +1,29 @@
+import { useContext } from "react";
 import NavItem from "./NavItem";
 import { ReactComponent as CartIcon } from "../../common/assets/cart.svg";
+import { CartContext } from "../../common/providers/CartProvider";
+
+function CartIconWithNumber({ productCount }) {
+  return (
+    <>
+      <CartIcon className="w-8 h-8" />
+      {productCount && (
+        <span className="absolute text-yellow-400 font-bold text-xs top-1/4 right-1/3">
+          {productCount}
+        </span>
+      )}
+    </>
+  );
+}
 
 function CartNavItem() {
+  const { cart } = useContext(CartContext);
+
   return (
     <NavItem
-      content={<CartIcon className="w-8 h-8" />}
+      content={<CartIconWithNumber productCount={cart?.products?.length} />}
       route="/cart"
-      className="ml-auto mr-2"
+      className="ml-auto mr-2 relative"
     />
   );
 }
