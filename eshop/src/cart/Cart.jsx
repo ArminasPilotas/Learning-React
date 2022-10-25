e60a5386c5cfd4c7b8cab236b3ee0c1d6af132ee
Arminas Pilotas <apilotas@reiz.tech>
2022-10-25 21:20:07 +0300

    Added FormControl component, make small form in cart

--- eshop/src/cart/Cart.jsx
+++ eshop/src/cart/Cart.jsx
@@ -1,14 +1,18 @@
-import { useContext, useEffect } from "react";
-import { CartContext } from "../common/providers/CartProvider";
+import FormControl from "../common/components/FormControl";
+import Input from "../common/components/Input";
 
 function Cart() {
-  const { cart, refreshData } = useContext(CartContext);
+  return (
+    <div className="m-auto" style={{ maxWidth: 500 }}>
+      <FormControl htmlFor="full-name" label="Full Name">
+        <Input id="full-name" />
+      </FormControl>
 
-  useEffect(() => {
-    refreshData();
-  }, []); //eslint-disable-line
-
-  return <div>Cart page {JSON.stringify(cart)}</div>;
+      <FormControl htmlFor="address" label="Address">
+        <Input id="address" />
+      </FormControl>
+    </div>
+  );
 }
 
 export default Cart;
